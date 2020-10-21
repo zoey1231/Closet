f5e655dc687bdc3bd954b6dade8a79cc5ae6d6f3
steveny9911 <stevenywy@gmail.com>
2020-10-20 23:50:04 -0700

    fix auth token

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -7,7 +7,7 @@ const LOG = require('../utils/logger');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       throw new Error('Authentication failed!');
     }
