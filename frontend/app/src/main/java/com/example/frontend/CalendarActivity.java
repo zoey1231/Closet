852fdd72fd8978a44e9898de631eb2b60cc57919
HotMoo <giotto0412@gmail.com>
2020-10-22 02:44:54 -0700

    Obtaining OAuth 2.0 access tokens

--- frontend/app/src/main/java/com/example/frontend/CalendarActivity.java
+++ frontend/app/src/main/java/com/example/frontend/CalendarActivity.java
@@ -1,126 +1,57 @@
 package com.example.frontend;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.CalendarScopes;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.appdatasearch.GetRecentContextCall;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class CalendarActivity extends AppCompatActivity {
-    // Google Calender API example
-    static final String APPLICATION_NAME = "Closet";
-    static final String TOKENS_DIRECTORY_PATH = "tokens";
-    static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
-    static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
+    // Google Calender API example
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    protected void onCreate(Bundle savedInstenceState) {
-        super.onCreate(savedInstenceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_calendar);
 
-        // Build a new authorized API client service
-        NetHttpTransport HTTP_TRANSPORT = null;
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Calendar service = null;
-        try {
-            service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                    .setApplicationName(APPLICATION_NAME)
-                    .build();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // create code verifier
+        String code_verifier = "5d2309e5bb73b864f989753887fe52f79ce5270395e25862da6940d5";
+        // create code challenge
+        String code_challenge = "MChCW5vD-3h03HMGFZYskOSTir7II_MMTb8a9rJNhnI";
 
-        // List the next 10 events from the primary calendar.
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = null;
-        try {
-            events = service.events().list("primary")
-                    .setMaxResults(10)
-                    .setTimeMin(now)
-                    .setOrderBy("startTime")
-                    .setSingleEvents(true)
-                    .execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        List<Event> items = events.getItems();
-        if (items.isEmpty()) {
-            System.out.println("No upcoming events found.");
-        } else {
-            System.out.println("Upcoming events");
-            for (Event event : items) {
-                DateTime start = event.getStart().getDateTime();
-                if (start == null) {
-                    start = event.getStart().getDate();
-                }
-                System.out.printf("%s (%s)\n", event.getSummary(), start);
+        // create authorization URL
+        String url = "https://www.googleapis.com/auth/" +
+                "scope=calendar&" +
+                "response_type=code&" +
+                "state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2.example.com%2Ftoken&" +
+                "redirect_uri=com.example.frontend%3A/oauth2redirect&" +
+                "client_id=544548915743-ffk9jah2jl5frc43h7hv43b3eod7gmum.apps.googleusercontent.com";
+
+        // send request to OAuth2.0 server
+        final TextView textView = (TextView) findViewById(R.id.text);
+        RequestQueue queue = new RequestQueue(this);
+        StringRequest stringRequest = new StringRequest(GetRecentContextCall.Request.Method.GET, url,
+                new GetRecentContextCall.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new GetRecentContextCall.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
             }
-        }
-    }
-
-    private static Credential getCredentials ( final NetHttpTransport HTTP_TRANSPORT) throws
-    IOException {
-        // Load client secrets.
-        InputStream in = MainActivity.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            try {
-                throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        GoogleClientSecrets clientSecrets = null;
-        try {
-            clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = null;
-        try {
-            flow = new GoogleAuthorizationCodeFlow.Builder(
-                    HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                    .setAccessType("offline")
-                    .build();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        try {
-            return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
     }
 }
