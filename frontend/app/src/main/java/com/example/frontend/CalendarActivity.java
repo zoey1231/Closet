ca9f077a65172d10bc5644186998d34b0cb51523
HotMoo <giotto0412@gmail.com>
2020-10-24 02:17:34 -0700

    Implemented API call using okhttp3

--- frontend/app/src/main/java/com/example/frontend/CalendarActivity.java
+++ frontend/app/src/main/java/com/example/frontend/CalendarActivity.java
@@ -2,56 +2,149 @@ package com.example.frontend;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.google.android.gms.appdatasearch.GetRecentContextCall;
+import java.io.IOException;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    // Google Calender API example
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_calendar);
 
-        // create code verifier
-        String code_verifier = "5d2309e5bb73b864f989753887fe52f79ce5270395e25862da6940d5";
-        // create code challenge
-        String code_challenge = "MChCW5vD-3h03HMGFZYskOSTir7II_MMTb8a9rJNhnI";
+        // obtain OAuth 2.0 access token
+        String oauth_response = null;
+        oauthRequest(oauth_response);
+        String access_token = tokenAccess(oauth_response);
 
-        // create authorization URL
-        String url = "https://www.googleapis.com/auth/" +
-                "scope=calendar&" +
-                "response_type=code&" +
-                "state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2.example.com%2Ftoken&" +
-                "redirect_uri=com.example.frontend%3A/oauth2redirect&" +
-                "client_id=544548915743-ffk9jah2jl5frc43h7hv43b3eod7gmum.apps.googleusercontent.com";
+        if (access_token != null) {
+            // call Google Calendar API
+            callApi(access_token);
+            String calendar_id = getCalendarId();
+            if (calendar_id != null) {
+                // add event
 
-        // send request to OAuth2.0 server
-        final TextView textView = (TextView) findViewById(R.id.text);
-        RequestQueue queue = new RequestQueue(this);
-        StringRequest stringRequest = new StringRequest(GetRecentContextCall.Request.Method.GET, url,
-                new GetRecentContextCall.Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: " + response.substring(0, 500));
-                    }
-                }, new GetRecentContextCall.Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
             }
-        });
+
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void oauthRequest(String oauth_response) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("scope", "calendar") //?need to pass in calendar.events as well
+                .addFormDataPart("client_id", "544548915743-ffk9jah2jl5frc43h7hv43b3eod7gmum.apps.googleusercontent.com")
+                .addFormDataPart("response_type", "code")
+                .addFormDataPart("redirect_uri", "urn:ietf:wg:oauth:2.0:oob")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://www.googleapis.com/auth")
+                .post(requestBody)
+                .build();
+
+        // get OAuth 2.0 response
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            oauth_response = response.header("code");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String tokenAccess(String oauth_response) {
+        // terminate if OAuth 2.0 response is error
+        if (oauth_response == null) {
+            return null;
+        }
+
+        // exchange authorization code for refresh and access tokens
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("code", oauth_response)
+                .addFormDataPart("client_id", "544548915743-46rmudne8rdgibohnh3nf4j1hr19sehh.apps.googleusercontent.com")
+                .addFormDataPart("client_secret", "SK0BUZzc9FAGha7GvMmBPe7I")
+                .addFormDataPart("client_redirect_uri", "https://closet-293003.firebaseapp.com/__/auth/handler")
+                .addFormDataPart("grant_type", "authorization_code")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://oauth2.googleapis.com")
+                .post(requestBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.header("access_token");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void callApi(String accress_token) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("Authorization: Bearer", accress_token)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://www.googleapis.com")
+                .post(requestBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String getCalendarId() {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://www.googleapis.com/calendar/v3/users/me/calendarList")
+                .post(requestBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.header("id");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
