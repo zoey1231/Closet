3c09c9d9b935049e1e9ecf83c395fd1c5d59388b
HotMoo <giotto0412@gmail.com>
2020-10-27 09:37:54 -0700

    Push before merging

--- frontend/app/src/main/java/com/example/frontend/Event.java
+++ frontend/app/src/main/java/com/example/frontend/Event.java
@@ -2,23 +2,23 @@ package com.example.frontend;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.threeten.bp.LocalDate;
 import java.util.Locale;
 
 import android.graphics.Bitmap;
 
 public class Event {
 
-    private Date date;
+    private LocalDate date;
     private String events;
 
-    public Event(Date date, String events) {
+    public Event(LocalDate date, String events) {
         this.date = date;
         this.events = events;
 
     }
 
-    public Date getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
--- frontend/app/src/main/java/com/example/frontend/ui/clothes/ClothesFragment.java
+++ frontend/app/src/main/java/com/example/frontend/ui/clothes/ClothesFragment.java
@@ -5,16 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.frontend.R;
-import com.example.frontend.ui.dashboard.DashboardViewModel;
 
 public class ClothesFragment extends Fragment implements View.OnClickListener {
 
