afdd506d6870ca70d80aa265571359c1453a4da5
Kay <stevenywy@gmail.com>
2020-10-29 12:33:58 -0700

    versionKey false (#34)
    * versionKey false
    
    * remove image_url

--- backend/model/clothes.js
+++ backend/model/clothes.js
@@ -1,48 +1,51 @@
 const mongoose = require('mongoose');
 
-const clothesSchema = mongoose.Schema({
-  // id will be mongodb generated id
+const clothesSchema = mongoose.Schema(
+  {
+    // id will be mongodb generated id
 
-  // required from request
-  category: {
-    type: String,
-    required: true,
-  },
-  color: {
-    type: String,
-    required: true,
-  },
-  seasons: [
-    {
-      type: String,
-      required: true,
-      enum: ['Spring', 'Summer', 'Fall', 'Winter', 'All'],
-    },
-  ],
-  occasions: [
-    {
+    // required from request
+    category: {
       type: String,
       required: true,
     },
-  ],
+    color: {
+      type: String,
+      required: true,
+    },
+    seasons: [
+      {
+        type: String,
+        required: true,
+        enum: ['Spring', 'Summer', 'Fall', 'Winter', 'All'],
+      },
+    ],
+    occasions: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
 
-  // optional
-  name: {
-    type: String,
-    required: false,
-  },
+    // optional
+    name: {
+      type: String,
+      required: false,
+    },
 
-  // set by backend
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+    // set by backend
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    updated: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  updated: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 clothesSchema.set('toJSON', {
   transform: (document, returnedObject) => {
