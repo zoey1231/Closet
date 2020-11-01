d4d9dfa3c711cb13b1d0e44ee79d233a9fff96e3
HotMoo <giotto0412@gmail.com>
2020-10-31 22:23:24 -0700

    Implemented upload image

--- frontend/app/src/main/java/com/example/frontend/AddClothesActivity.java
+++ frontend/app/src/main/java/com/example/frontend/AddClothesActivity.java
@@ -1,10 +1,14 @@
 package com.example.frontend;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaSync;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,19 +17,39 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.frontend.ui.clothes.ClothesFragment;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class AddClothesActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "AppClothesActivity";
+
     private ImageView image;
     private ImageButton buttonImage;
     private Button buttonSave;
     private TextView textAdd;
-    private static final int IMAGE = 1;
+
+    private static final int ADD = 1;
+
+    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +71,18 @@ public class AddClothesActivity extends AppCompatActivity implements View.OnClic
             case R.id.button_image_add:
                 buttonImage.setVisibility(View.GONE);
                 textAdd.setVisibility(View.GONE);
-                Intent intentImage = new Intent(Intent.ACTION_PICK);
-                intentImage.setType("image/*");
-                startActivityForResult(intentImage, IMAGE);
+                Intent intentAdd = new Intent(Intent.ACTION_PICK);
+                intentAdd.setType("image/*");
+                startActivityForResult(intentAdd, ADD);
                 break;
+
             case R.id.button_save_add:
-                Intent intentSave = new Intent(AddClothesActivity.this, ClothesFragment.class);
-                startActivity(intentSave);
+//                FragmentManager manager = getSupportFragmentManager();
+//                FragmentTransaction transaction = manager.beginTransaction();
+//                ClothesFragment fragment = new ClothesFragment();
+////                fragment.setArguments(bundle);
+//                transaction.replace(R.id.nav_host_fragment_container, fragment);
+//                transaction.commit();
                 break;
         }
     }
@@ -62,19 +91,63 @@ public class AddClothesActivity extends AppCompatActivity implements View.OnClic
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK) {
-            try {
-                final Uri uri = data.getData();
-                final InputStream stream = getContentResolver().openInputStream(uri);
-                final Bitmap bitmap = BitmapFactory.decodeStream(stream);
-                image.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(AddClothesActivity.this, "Something went wrong", Toast.LENGTH_LONG);
+        if (requestCode == ADD) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri uri = data.getData();
+                    final InputStream stream = getContentResolver().openInputStream(uri);
+                    final Bitmap bitmap = BitmapFactory.decodeStream(stream);
+                    image.setImageBitmap(bitmap);
+//                    bundle.putString("uri", String.valueOf(uri));
+                    String path = getPath(uri);
+                    File file = new File(path);
+                    sendImageToServer(file);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(AddClothesActivity.this, "Something went wrong", Toast.LENGTH_LONG);
+                }
+            } else {
+                Toast.makeText(AddClothesActivity.this, "You haven't picked image", Toast.LENGTH_LONG);
             }
         }
-        else {
-            Toast.makeText(AddClothesActivity.this, "You haven't picked image", Toast.LENGTH_LONG);
-        }
     }
+
+    private String getPath(Uri uri) {
+        String[] projection = {MediaStore.MediaColumns.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(index);
+    }
+
+    private void sendImageToServer(File file) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image_url", file.getName(), RequestBody.create(MediaType.parse("image/*"), file))
+                .build();
+        Request request = new Request.Builder()
+                .url("http://closet-cpen321.westus.cloudapp.azure.com/api/clothes")
+                .post(body)
+                .build();
+        Log.d(TAG,"prepared to sendImageToServer");
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+            }
+        });
+
+        Log.d(TAG,"finished sendImageToServer");
+    }
+
 }
--- frontend/app/src/main/java/com/example/frontend/ui/clothes/ClothesFragment.java
+++ frontend/app/src/main/java/com/example/frontend/ui/clothes/ClothesFragment.java
@@ -2,10 +2,15 @@ package com.example.frontend.ui.clothes;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -16,10 +21,15 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.frontend.AddClothesActivity;
 import com.example.frontend.R;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class ClothesFragment extends Fragment implements View.OnClickListener {
 
     private ClothesViewModel clothesViewModel;
-    private ImageButton imageButton;
+    private ImageButton buttonAdd;
+    private ImageView image;
+    private GridLayout grid;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,8 +37,25 @@ public class ClothesFragment extends Fragment implements View.OnClickListener {
                 ViewModelProviders.of(this).get(ClothesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_clothes, container, false);
 
-        imageButton = root.findViewById(R.id.button_add);
-        imageButton.setOnClickListener(this);
+        buttonAdd = root.findViewById(R.id.button_add);
+        image = root.findViewById(R.id.image_add);
+        grid = root.findViewById(R.id.grid);
+        buttonAdd.setOnClickListener(this);
+
+//        Uri uri = Uri.parse(getArguments().getString("uri"));
+//        try {
+//            InputStream stream = getActivity().getContentResolver().openInputStream(uri);
+//            Bitmap bitmap = BitmapFactory.decodeStream(stream);
+//            image.setImageBitmap(bitmap);
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+//        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+//        params.rowSpec = GridLayout.spec(0);
+//        params.columnSpec = GridLayout.spec(0);
+//        grid.addView(image, params);
 
         return root;
     }
