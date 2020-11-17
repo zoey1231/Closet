f4eb2fa180e5b44661c6237df840feadd1fd9aee
zhiwenli <lizhiwenzg@gmail.com>
2020-11-16 23:49:27 -0800

    Added user profile update tests

--- backend/__tests__/controller/users.test.js
+++ backend/__tests__/controller/users.test.js
@@ -1,4 +1,3 @@
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const http = require('http');
@@ -6,8 +5,6 @@ const http = require('http');
 const app = require('../../app');
 
 describe('Authentication Tests', () => {
-  const testToken = 'testToken';
-
   let server, api;
   beforeAll(done => {
     server = http.createServer(app);
@@ -122,7 +119,7 @@ describe('Authentication Tests', () => {
     );
   });
 
-  it('200 get user profile', async () => {
+  it('200 should get user profile', async () => {
     const res = await api
       .get('/api/users/me')
       .set('Authorization', `Bear ${token}`);
@@ -131,6 +128,90 @@ describe('Authentication Tests', () => {
     expect(res.body.id).toEqual(userId);
     expect(res.body.email).toEqual('testing@testing.com');
     expect(res.body.city).toEqual('vancouver');
+  });
+
+  it('200 should update user profile if inputs are all correct', async () => {
+    const updatedProfile = {
+      name: 'NEW TESTING',
+      email: 'newtesting@newtesting.com',
+      city: 'burnaby',
+    };
+
+    const res = await api
+      .put('/api/users/me')
+      .set('Authorization', `Bear ${token}`)
+      .send(updatedProfile);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.updatedUser.id).toEqual(userId);
+    expect(res.body.updatedUser.email).toEqual(updatedProfile.email);
+    expect(res.body.updatedUser.city).toEqual(updatedProfile.city);
+  });
+
+  it('422 should fail to update user profile if inputs are not valid', async () => {
+    const updatedProfile = {
+      name: 'NEW TESTING',
+      email: 'newtesting@newtesting.com',
+    };
+
+    const res = await api
+      .put('/api/users/me')
+      .set('Authorization', `Bear ${token}`)
+      .send(updatedProfile);
+
+    expect(res.statusCode).toEqual(422);
+    expect(res.body.message).toEqual(
+      'Invalid inputs passed, please check your data.'
+    );
+  });
+
+  it('500 should fail to update user profile if city is not found', async () => {
+    const updatedProfile = {
+      name: 'NEW TESTING',
+      email: 'newtesting@newtesting.com',
+      city: 'a',
+    };
+
+    const res = await api
+      .put('/api/users/me')
+      .set('Authorization', `Bear ${token}`)
+      .send(updatedProfile);
+
+    expect(res.statusCode).toEqual(500);
+    expect(res.body.message).toEqual(
+      'Cannot find your city, please check and try again'
+    );
+  });
+
+  it('422 should fail to update user profile if the email has been registered already', async () => {
+    const otherUser = {
+      name: 'TESTING',
+      email: 'othertesting@othertesting.com',
+      password: 'TESTING',
+    };
+
+    let res = await api.post('/api/users/signup').send(otherUser);
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body.email).toEqual(otherUser.email.toLowerCase());
+    expect(res.body.userId).toBeTruthy();
+    expect(res.body.token).toBeTruthy();
+
+    const updatedProfile = {
+      name: 'NEW TESTING',
+      email: 'othertesting@othertesting.com',
+      city: 'vancouver',
+    };
+
+    res = await api
+      .put('/api/users/me')
+      .set('Authorization', `Bear ${token}`)
+      .send(updatedProfile);
+
+    expect(res.statusCode).toEqual(422);
+    expect(res.body.message).toEqual(
+      'The email has been registered, please change to another one'
+    );
   });
 
   afterAll(async done => {
