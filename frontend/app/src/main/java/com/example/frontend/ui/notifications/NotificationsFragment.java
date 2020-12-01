cf5db1e15191e3af84066c13e47943b5887ff21f
HotMoo <giotto0412@gmail.com>
2020-11-30 21:48:21 -0800

    Added click button effect

--- frontend/app/src/main/java/com/example/frontend/ui/notifications/NotificationsFragment.java
+++ frontend/app/src/main/java/com/example/frontend/ui/notifications/NotificationsFragment.java
@@ -24,7 +24,6 @@ import com.example.frontend.R;
 import com.example.frontend.RegisterActivity;
 import com.example.frontend.ServerCommAsync;
 import com.example.frontend.User;
-import com.example.frontend.ui.home.HomeFragment;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
@@ -65,7 +64,7 @@ public class NotificationsFragment extends Fragment implements View.OnClickListe
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
 
         userEmail = root.findViewById(R.id.tv_userEmail);
