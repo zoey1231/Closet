dc1ea532fb9ef55ff73f43edfaaaf2373af53b6e
Summer Sun <51576044+giotto0412@users.noreply.github.com>
2020-12-03 19:40:54 -0800

    Fiexed codacy issue

--- frontend/app/src/main/java/com/example/frontend/LoginActivity.java
+++ frontend/app/src/main/java/com/example/frontend/LoginActivity.java
@@ -5,7 +5,6 @@ import androidx.test.espresso.idling.CountingIdlingResource;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
